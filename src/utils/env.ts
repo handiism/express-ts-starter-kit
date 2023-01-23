d3afa73ce6669052f1d7ed69549479fc6d6b5d99
handiism <mail.handira@gmail.com>
2023-01-23 12:51:47 +0700

    chore: minify env file

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,6 +2,6 @@ import { config } from "dotenv";
 
 config();
 
-export const protocol = String(process.env.PROTOCOL) || "http";
-export const hostname = String(process.env.HOSTNAME) || "127.0.0.1";
-export const port = Number(process.env.PORT) || 8080;
+export const protocol = String(process.env.APP_PROTOCOL) || "http";
+export const hostname = String(process.env.APP_HOSTNAME) || "127.0.0.1";
+export const port = Number(process.env.APP_PORT) || 8080;
